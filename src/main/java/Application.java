585f90e35c0d25901e242dae79759f8f7317bbe9
nuttren <roman.karpencko@gmail.com>
2023-04-17 10:26:18 +0400

    test

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Application {
+    public static void main(String[] args) {
+        final String user = "postgres";
+        final String password = "1";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+        try (final Connection connection =
+                     DriverManager.getConnection(url, user, password)) {
+
+            System.out.println("Соединение установлено!");
+
+        } catch (SQLException e) {
+            System.out.println("Ошибка при подключении к базе данных!");
+            e.printStackTrace();
+            // Исключение для обработки возможных ошибок при подключении
+        }
+    }
 }
+
